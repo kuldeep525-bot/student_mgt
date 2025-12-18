f1a1378d2464136f2dbc01d050f4faf5fa2581f9
kuldeep kumar <sushma97797@gmail.com>
2025-12-18 13:48:36 +0530

    addeed feature

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ app.get("/",(req,res)=>{
   res.send("First")
 })
 
+// feature
+app.get("/feature",(req,res)=>{
+  res.send("second")
+})
+
 
 app.listen(4000,()=>{
   console.log("app running at port : ", 4000)
