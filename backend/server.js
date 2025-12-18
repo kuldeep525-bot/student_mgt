14b350d2c287f95a059cefae3818b4ea0bab24bf
kuldeep kumar <sushma97797@gmail.com>
2025-12-18 13:43:42 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const express =require('express');
+const app=express();
+
+app.get("/",(req,res)=>{
+  res.send("First")
+})
+
+
+app.listen(4000,()=>{
+  console.log("app running at port : ", 4000)
+
+})
